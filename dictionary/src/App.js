9f17b01a594955d23ae7ffea8612cbd00a87c2db
DianaB177 <anaidb77@gmail.com>
2022-08-14 23:08:23 +0200

    start app

--- dictionary/src/App.js
+++ dictionary/src/App.js
@@ -1,10 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
      <div>Holas</div>
+     <a href='/' className='btn btn-primary' rel='noopener noreferrer'>Chau</a>
       </header>
     </div>
   );
